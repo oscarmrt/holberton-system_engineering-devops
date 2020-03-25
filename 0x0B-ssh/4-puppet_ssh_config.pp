8cb0be56117c8a6f3e65ae95dadf96e6ce45c9bc
oscarmrt <oscarmrt@hotmail.com>
2020-03-25 01:24:56 +0000

    modified file 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # Let’s practice using Puppet to make changes to our configuration file.
 
-file_line {'Turn off password aut':
+file_line { 'Turn off password aut':
     ensure => present,
     path   => 'etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
     match  => 'PasswordAuthentication yes',
 }
 
-file_line {'Declaration identity file':
+file_line { 'Declaration identity file':
     ensure => present,
     path   => 'etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/holberton'
+    line   => 'IdentityFile ~/.ssh/holberton',
 }
