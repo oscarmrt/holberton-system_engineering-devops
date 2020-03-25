64d7b0508a90a150569870670c98c218ca298a2c
oscarmrt <oscarmrt@hotmail.com>
2020-03-25 01:37:18 +0000

    modified file 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# Let’s practice using Puppet to make changes to our configuration file.
+# set up your client SSH configuration file so that you can connect
+# to a server without typing a password using Puppet..
 
-file_line { 'Turn off password aut':
-    ensure => present,
-    path   => 'etc/ssh/ssh_config',
-    line   => ' PasswordAuthentication no',
-    match  => 'PasswordAuthentication yes',
-}
+file_line { 'Turn off passwd auth':
+	    ensure => present,
+	    path   => '/etc/ssh/ssh_config',
+	    line   => ' PasswordAuthentication no',
+	    match  => 'PasswordAuthentication yes'
+	  }
 
-file_line { 'Declaration identity file':
-    ensure => present,
-    path   => 'etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/holberton',
-}
+file_line { 'Declare identity file':
+	    ensure => present,
+	    path   => '/etc/ssh/ssh_config',
+	    line   => ' IdentityFile ~/.ssh/holberton'
+	  }
