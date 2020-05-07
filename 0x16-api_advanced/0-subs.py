a0e0fa90aea780d993ad53d07e5d141118d7fe99
oscarmrt <oscarmrt@hotmail.com>
2020-05-07 17:13:14 +0000

    file 0 and 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that returns subscribers"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = 'https://www.reddit.com/r/{}/about.json'
+    headers = {'User-Agent':
+               'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/70.0.3538.77 Safari/537.36'}
+    r = requests.get(url.format(subreddit), headers=headers).json()
+    subs = r.get('data', {}).get('subscribers', 0)
+    return subs
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a
+    given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+    url = 'https://www.reddit.com/r/{}/hot.json'
+    headers = {'User-Agent':
+               'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/70.0.3538.77 Safari/537.36'}
+    r = requests.get(url.format(subreddit), headers=headers,
+                     params={'limit': 10}).json()
+    tenPosts = r.get('data', {}).get('children', None)
+    for post in tenPosts:
+        print(post.get('data', {}).get('title', None))
