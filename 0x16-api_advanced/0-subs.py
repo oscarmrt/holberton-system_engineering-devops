f1aef4610829a5424108f29b4c3f7e38de6f205d
oscarmrt <oscarmrt@hotmail.com>
2020-05-07 17:17:44 +0000

    modified file 0 and 1 adding comments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''queries the Reddit API and returns the number of subscribers'''
 
 import requests
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit'''
 
 import requests
 
